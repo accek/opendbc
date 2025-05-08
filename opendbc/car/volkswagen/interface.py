b73e7a0c4baa0a71c92ce831745dac1d550859aa
Szymon Acedański <accek@mimuw.edu.pl>
2025-05-08 20:35:23 +0200

    vw: tune

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -82,7 +82,7 @@ class CarInterface(CarInterfaceBase):
         ret.minEnableSpeed = 4.5
       ret.longitudinalActuatorDelay = 0.5 # s
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
+      ret.longitudinalTuning.kiV = [1.2, 0.4, 0.25]
 
     ret.pcmCruise = not ret.openpilotLongitudinalControl
     ret.stopAccel = -0.55
